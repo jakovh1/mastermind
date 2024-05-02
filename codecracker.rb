3e214f6cda542ef9d67ac7680c6f5141c769de1a
Jakov <1jakov.1horvat@gmail.com>
2024-05-02 16:55:08 +0200

    Refactored Code and Efficiency Improved

--- codecracker.rb
+++ codecracker.rb
@@ -22,6 +22,7 @@ class Codecracker
     @present_colors = []
     @pegs_registered = 0
     @used_answers = []
+    @possible_codes = []
   end
 
   # The following function takes guess from an user
@@ -63,12 +64,9 @@ class Codecracker
       @present_colors.push(@used_colors[@used_colors.length - 1])
     end
 
-    if (@pegs_for_computer[:red] + @pegs_for_computer[:white]) > @pegs_registered
-      @pegs_registered = @pegs_for_computer[:red] + @pegs_for_computer[:white]
-    end
+    return unless @pegs_for_computer[:red] + @pegs_for_computer[:white] > @pegs_registered
 
-    @pegs_for_computer[:red] = 0
-    @pegs_for_computer[:white] = 0
+    @pegs_registered = @pegs_for_computer[:red] + @pegs_for_computer[:white]
   end
 
   # When all correct colors are collected
@@ -77,25 +75,56 @@ class Codecracker
   def shuffle_guess
     @used_answers.push(@guess) if @used_answers.empty?
 
+    generate_possible_codes if @pegs_for_computer[:red] == 2 && @possible_codes.empty?
+
     loop do
-      @guess = @present_colors.shuffle.join(' ')
+      @guess = @possible_codes.empty? ? @present_colors.shuffle.join(' ') : @possible_codes.shift.join(' ')
       break unless @used_answers.include?(@guess)
     end
 
+    @pegs_for_computer[:red] = 0
+    @pegs_for_computer[:white] = 0
     @used_answers.push(@guess)
     @guess
   end
 
   # This method will generate a guess when 4 correct colors have not been collected yet
   def generate_guess
-    color = @colors_for_computer.shuffle.sample.to_s
-    how_many_times_new_color = 4 - @pegs_registered
-
     @guess = ''
 
     @present_colors.each do |element|
       @guess += "#{element} "
     end
+
+    add_new_color_to_guess
+
+    @pegs_for_computer[:red] = 0
+    @pegs_for_computer[:white] = 0
+
+    @guess = @guess.rstrip
+  end
+
+  private
+
+  # The following method will generate all possible codes
+  def generate_possible_codes
+    (0..2).each do |i|
+      (i + 1..3).each do |j|
+        splitted_guess = @guess.split(' ')
+
+        aux = splitted_guess[i]
+        splitted_guess[i] = splitted_guess[j]
+        splitted_guess[j] = aux
+
+        @possible_codes.push(splitted_guess)
+      end
+    end
+  end
+
+  # The following method adds a new color when generating a guess
+  def add_new_color_to_guess
+    color = @colors_for_computer.shuffle.sample.to_s
+    how_many_times_new_color = 4 - @pegs_registered
 
     how_many_times_new_color.times do
       @guess += "#{color} "
@@ -103,10 +132,5 @@ class Codecracker
 
     @used_colors.push(color)
     @colors_for_computer.delete(color)
-
-    @pegs_for_computer[:red] = 0
-    @pegs_for_computer[:white] = 0
-
-    @guess = @guess.rstrip
   end
 end
